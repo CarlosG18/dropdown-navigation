4a46f4df24bf1e5453199265cd7f16a209ea4a73
CarlosG18 <c.gabriel.abc18@gmail.com>
2023-08-06 16:02:16 -0300

    fineshed"

---
+++ js/main.js
@@ -0,0 +1,40 @@
+const dropMenu = document.querySelectorAll('.nav-menu-drop')
+const btnDrop = document.querySelectorAll('.nav-item-drop')
+const btnOpen = document.querySelectorAll('.icon')
+const nav = document.querySelector('nav')
+const navMenu = document.querySelectorAll('.nav-menu')
+const main = document.querySelector('main')
+const ilustration = document.querySelector('.ilustration')
+
+window.addEventListener('resize', () => {
+    let widthScreen = window.innerWidth
+
+    if(widthScreen > 800){
+        dropMenu.forEach(element => {
+            element.classList.add('position-abs')
+        })
+       // ilustration.src = "{% static 'images/image-hero-desktop.png' %}"
+    }else{
+       // ilustration.src = "{% static 'images/image-hero-mobile.png' %}"
+    }
+})
+
+btnOpen.forEach(btn=> {
+    btn.addEventListener("click", () => {
+        nav.classList.toggle('active')
+        main.classList.toggle('active')
+        })
+})
+
+btnDrop.forEach((element, index) => {
+    element.addEventListener('click', () => {
+        let widthScreen = window.innerWidth
+        
+        dropMenu[index].classList.toggle('active')     
+        if(widthScreen < 800){
+            dropMenu[index].classList.remove('position-abs')
+        }
+        
+    })
+
+})
